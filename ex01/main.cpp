a715d6e997f8b1ed6244bc892cf28ca9d124a8ab
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-01 11:59:47 +0200

    starting the main process with some display informaitons

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,19 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:16:16 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/01 10:16:17 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 11:58:58 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+void displayPhonebook()
+{
+    std::cout << "\n==== Welcome to your custom Phonebook made by Hichem ====\n";
+    std::cout << "Available commands:\n";
+    std::cout << "ADD    _ Add a new contact\n";
+    std::cout << "SEARCH _ Search for an existing contact\n";
+    std::cout << "EXIT   _ Exit the program\n";
+    std::cout <<"==============================\n";
+}
 
