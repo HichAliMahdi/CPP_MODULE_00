ce0dcd481b51357d3771b7d7662957bd5332b7e0
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-01 12:12:34 +0200

    finalizing the logic in the main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:16:16 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/01 11:58:58 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 12:12:08 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,65 @@
 
 void displayPhonebook()
 {
-    std::cout << "\n==== Welcome to your custom Phonebook made by Hichem ====\n";
-    std::cout << "Available commands:\n";
-    std::cout << "ADD    _ Add a new contact\n";
-    std::cout << "SEARCH _ Search for an existing contact\n";
-    std::cout << "EXIT   _ Exit the program\n";
-    std::cout <<"==============================\n";
+	std::cout << "\n==== Welcome to your custom Phonebook made by Hichem ====\n";
+	std::cout << "Available commands:\n";
+	std::cout << "ADD    _ Add a new contact\n";
+	std::cout << "SEARCH _ Search for an existing contact\n";
+	std::cout << "EXIT   _ Exit the program\n";
+	std::cout <<"==============================\n";
 }
 
+Contact createContact()
+{
+	Contact     newContact;
+	std::string input;
+
+	std::cout << "Enter the first name: ";
+	std::getline(std::cin, input);
+	newContact.setFirstName(input);
+
+	std::cout << "Enter the last name: ";
+	std::getline(std::cin, input);
+	newContact.setLastName(input);
+
+	std::cout << "Enter the nickname: ";
+	std::getline(std::cin, input);
+	newContact.setNickName(input);
+
+	std::cout << "Enter the phone number: ";
+	std::getline(std::cin, input);
+	newContact.setPhoneNumber(input);
+
+	std::cout << "Enter the darkest secret: "
+	std::getline(std::cin, input);
+	newContact.setDarkestSecret(input);
+
+	return (newContact);
+}
+
+int main()
+{
+	Phonebook   phonebook;
+	std::string	command;
+	
+	std::cout << "My phonebook\n";
+	displayPhonebook();
+	std::getline(std::cin, command);
+	if (command == "ADD")
+	{
+		Contact contact = createContact();
+		phonebook.addContact(contact);
+	}
+	else if (command == "SEARCH")
+	{
+		phonebook.searchContact();
+	}
+	else if (command == "EXIT")
+	{
+		std::cout << "Goodbye! All the contacts will be lost forever!\n";
+		break ;
+	}
+	else
+		std::cout << "Unknown command, try again.\n";
+	return (0);
+}
