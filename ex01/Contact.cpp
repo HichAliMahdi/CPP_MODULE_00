33a1dcaca3865597a8a9a62d9f5d312d46ccc3a7
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-01 11:05:02 +0200

    finished the Contact part, next step the Phonebook part

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:15:00 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/01 10:47:26 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 11:03:14 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,52 @@ Contact::Contact()
     _darkestsecret = "";
 }
 
+void    Contact::setFirstName(std::string firstName)
+{
+    _firstname = firstName;
+}
+
+void    Contact::setLastName(std::string lastName)
+{
+    _lastname = lastName;
+}
+
+void    Contact::setNickName(std::string nickName)
+{
+    _nickname = nickName;
+}
+
+void    Contact::setPhoneNumber(std::string phoneNumber)
+{
+    _phonenumber = phoneNumber;
+}
+
+void    Contact::setDarkestSecret(std::string darkestSecret)
+{
+    _darkestsecret = darkestSecret;
+}
+
+std::string	Contact::getFirstName() const
+{
+    return _firstname;
+}
+
+std::string Contact::getLastName() const
+{
+    return _lastname;
+}
+
+std::string Contact::getNickName() const
+{
+    return _nickname;
+}
+
+std::string Contact::getPhoneNumber() const
+{
+    return _phonenumber;
+}
+
+std::string Contact::getDarkestSecret() const
+{
+    return _darkestsecret;
+}
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 09:43:15 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/01 10:44:44 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 11:03:27 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,25 @@
 
 class Contact
 {
-    public:
-        Contact();
-        void    setFirstName(std::string firstName);
-        void    setLastName(std::string lastName);
-        void    setNickName(std::string nickName);
-        void    setPhoneNumber(std::string phoneNunber);
-        void    setDarkestSecret(std::string darkestSecret);
-        
-    private:
-        std::string _firstname;
-        std::string _lastname;
-        std::string _nickname;
-        std::string _phonenumber;
-        std::string _darkestsecret;
+	public:
+		Contact();
+		void		setFirstName(std::string firstName);
+		void		setLastName(std::string lastName);
+		void		setNickName(std::string nickName);
+		void		setPhoneNumber(std::string phoneNunber);
+		void		setDarkestSecret(std::string darkestSecret);
+		
+		std::string	getFirstName() const;
+		std::string getLastName() const;
+		std::string	getNickName() const;
+		std::string	getPhoneNumber() const;
+		std::string	getDarkestSecret() const;
+	private:
+		std::string _firstname;
+		std::string _lastname;
+		std::string _nickname;
+		std::string _phonenumber;
+		std::string _darkestsecret;
 };
 
 #endif
