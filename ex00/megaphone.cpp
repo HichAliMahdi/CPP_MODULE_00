b078d2d0ed9fa84345a0e6e5662bac78cdf50a74
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-01 10:16:42 +0200

    start learning classes and making a full structure of the ex01

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,24 +6,25 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 21:54:57 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/03/31 22:02:11 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 10:08:57 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
+using namespace std;
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
         for (int i = 1; i < argc; i++)
         {
             for (int j = 0; argv[i][j]; j++)
-            std::cout << (char)std::toupper(argv[i][j]);
+            cout << (char)std::toupper(argv[i][j]);
         }
-        std::cout << std::endl;
+        cout << endl;
     }
     return (0);
 }
