07f8534e8f0d650a5ba5dd1448e3c876ab18da78
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-01 16:10:37 +0200

    getting rid of the forbidden function

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,25 +6,24 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 21:54:57 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/01 10:08:57 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/01 16:09:48 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
-using namespace std;
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
         for (int i = 1; i < argc; i++)
         {
             for (int j = 0; argv[i][j]; j++)
-            cout << (char)std::toupper(argv[i][j]);
+            std::cout << (char)std::toupper(argv[i][j]);
         }
-        cout << endl;
+        std::cout << std::endl;
     }
     return (0);
 }
